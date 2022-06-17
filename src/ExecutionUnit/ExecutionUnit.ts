65774f0003ae53717b39ca252a04e0ee651055ad
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-06-17 10:35:00 -0400

    Create ExecutionUnit.ts

---
+++ src/ExecutionUnit/ExecutionUnit.ts
@@ -0,0 +1,13 @@
+import { Stack } from "../Stack/Stack.ts";
+
+export class ExecutionUnit {
+  constructor(private stack = new Stack(8)) {
+  }
+
+  i8Literal(value: number) {
+    if (value > 0xff) {
+      throw new RangeError("");
+    }
+    this.stack.push(value);
+  }
+}
