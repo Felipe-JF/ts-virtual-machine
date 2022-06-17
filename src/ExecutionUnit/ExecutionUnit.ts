c1f7575fc58fd21e097777096bfcc9aa47ea4dc7
Felipe-JF <87448021+Felipe-JF@users.noreply.github.com>
2022-06-17 11:52:28 -0400

    Update ExecutionUnit.ts
    created decrement instruction

--- src/ExecutionUnit/ExecutionUnit.ts
+++ src/ExecutionUnit/ExecutionUnit.ts
@@ -16,6 +16,15 @@ export class ExecutionUnit {
     return this.alu0(add);
   }
 
+  decrement0() {
+    return this.unary0(decrement);
+  }
+
+  private unary0(fn: (a: number) => number) {
+    this.stack.push(fn(this.stack.pop()));
+    return this;
+  }
+
   private alu0(fn: (a: number, b: number) => number) {
     const a = this.stack.pop();
     const b = this.stack.pop();
@@ -26,4 +35,8 @@ export class ExecutionUnit {
 
 function add(a: number, b: number) {
   return a + b;
+}
+
+function decrement(a: number) {
+  return a - 1;
 }
